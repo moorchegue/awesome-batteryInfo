514d102290f074d893d9afe4fd84cc2796a076b0
murchik <mixturchik@gmail.com>
2015-04-22 12:58:56 +0800

    Some defensive programming and updated paths.

--- battery.lua
+++ battery.lua
@@ -3,38 +3,49 @@
 -- on the "Gigamo Battery Widget" found in the wiki at awesome.naquadah.org
 
 local naughty = require("naughty")
+local beautiful = require("beautiful")
+
+
+function _batteryInfo(adapter)
+    local fcur = io.open("/sys/class/power_supply/"..adapter.."/charge_now")
+    local fcap = io.open("/sys/class/power_supply/"..adapter.."/charge_full")
+    local fsta = io.open("/sys/class/power_supply/"..adapter.."/status")
+    local cur = fcur:read()
+    local cap = fcap:read()
+    local sta = fsta:read()
+    fcur:close()
+    fcap:close()
+    fsta:close()
+    local battery = math.floor(cur * 100 / cap)
+
+    if sta:match("Charging") then
+        icon = "⚡"
+        percent = "%"
+    elseif sta:match("Discharging") then
+        icon = ""
+        percent = "%"
+        if tonumber(battery) < 10 then
+            naughty.notify({ title        = "Battery Warning"
+                         , text         = "Battery low!".."    "..battery..percent.."    ".."left!"
+                         , timeout    = 5
+                         , position = "top_right"
+                         , fg             = beautiful.fg_focus
+                         , bg             = beautiful.bg_focus
+            })
+        end
+    else
+        battery = "A/C"
+        icon = ""
+        percent = ""
+    end
+    return " "..icon..battery..percent.." "
+end
 
 function batteryInfo(adapter)
-  local fcur = io.open("/sys/class/power_supply/"..adapter.."/energy_now")  
-  local fcap = io.open("/sys/class/power_supply/"..adapter.."/energy_full")
-  local fsta = io.open("/sys/class/power_supply/"..adapter.."/status")
-  local cur = fcur:read()
-  local cap = fcap:read()
-  local sta = fsta:read()
-  fcur:close()
-  fcap:close()
-  fsta:close()
-  local battery = math.floor(cur * 100 / cap)
-
-  if sta:match("Charging") then
-    icon = "⚡"
-    percent = "%"
-  elseif sta:match("Discharging") then
-    icon = ""
-    percent = "%"
-    if tonumber(battery) < 15 then
-      naughty.notify({ title    = "Battery Warning"
-             , text     = "Battery low!".."  "..battery..percent.."  ".."left!"
-             , timeout  = 5
-             , position = "top_right"
-             , fg       = beautiful.fg_focus
-             , bg       = beautiful.bg_focus
-      })
+    local ok, battery = pcall(_batteryInfo, adapter)
+    if ok then
+        return battery
+    else
+        return "肏你妈"
     end
-  else
-    battery = "A/C"
-    icon = ""
-    percent = ""
-  end
-  return " "..icon..battery..percent.." "
 end
